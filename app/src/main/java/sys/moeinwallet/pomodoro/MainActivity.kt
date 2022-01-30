edbc82d85c7563c4b247a4dff03c9c777a0f8268
s.banikamali <ahmad.banikamali@gmail.com>
2022-01-30 12:59:43 +0330

    1- bottom navigation bar

---
+++ app/src/main/java/sys/moeinwallet/pomodoro/MainActivity.kt
@@ -0,0 +1,119 @@
+package sys.moeinwallet.pomodoro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Timer
+import androidx.compose.material.icons.rounded.LockClock
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import sys.moeinwallet.pomodoro.rewardlist.RewardListScreen
+import sys.moeinwallet.pomodoro.timer.TimerScreen
+import sys.moeinwallet.pomodoro.ui.theme.PomodoroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PomodoroTheme {
+                ScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenContent() {
+    val navController = rememberNavController()
+
+    Scaffold(bottomBar = {
+        BottomNavigation {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+
+            bottomNavDestinations.forEach { bottomNavigationItem ->
+                BottomNavigationItem(
+                    label = {
+                        Text(text = stringResource(id = bottomNavigationItem.label))
+                    },
+                    selected = currentDestination?.hierarchy?.any {
+                        bottomNavigationItem.route == it.route
+                    } == true,
+                    onClick = {
+                        navController.navigate(bottomNavigationItem.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    alwaysShowLabel = false,
+                    icon = {
+                        Icon(imageVector = bottomNavigationItem.icon,
+                            contentDescription = "")
+                    },
+
+                    )
+            }
+
+        }
+    }) { innerPadding ->
+
+        NavHost(navController = navController,
+            startDestination = "timer",
+            modifier = Modifier
+                .padding(innerPadding)) {
+            composable("timer") { TimerScreen() }
+            composable("rewardList") { RewardListScreen() }
+        }
+    }
+
+
+}
+
+val bottomNavDestinations = listOf<BottomNavDestination>(
+    BottomNavDestination.Timer,
+    BottomNavDestination.RewardList,
+)
+
+
+sealed class BottomNavDestination(
+    val route: String,
+    val icon: ImageVector,
+    @StringRes val label: Int,
+) {
+
+    object Timer : BottomNavDestination("timer", Icons.Outlined.Timer, R.string.timer)
+    object RewardList :
+        BottomNavDestination("rewardList", Icons.Outlined.List, R.string.reward_list)
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PomodoroTheme {
+        ScreenContent()
+    }
+}
---
+++ app/src/main/java/sys/moeinwallet/pomodoro/rewardlist/RewardListScreen.kt
@@ -0,0 +1,44 @@
+package sys.moeinwallet.pomodoro.rewardlist
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import sys.moeinwallet.pomodoro.R
+import sys.moeinwallet.pomodoro.ui.theme.PomodoroTheme
+
+
+@Composable
+fun RewardListScreen() {
+    ScreenContent()
+}
+
+@Composable
+private fun ScreenContent() {
+
+    Box(modifier = Modifier
+        .fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = stringResource(id = R.string.reward_list))
+    }
+}
+
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun ScreenContentPreview() {
+
+    PomodoroTheme {
+        Surface {
+            ScreenContent()
+        }
+    }
+}
